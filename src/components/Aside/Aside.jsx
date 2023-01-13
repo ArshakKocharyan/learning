8e23497921db958969bb3fcaeedd800f95f88691
gosh888 <goshhayrapetyan@gmail.com>
2023-01-13 21:41:56 +0400

    created aside component

---
+++ src/components/Aside/Aside.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import classNames from 'classnames/bind';
+import styles from './Aside.module.scss';
+
+const cx = classNames.bind(styles);
+
+export class Aside extends Component {
+  render() {
+    const { isOpen } = this.props;
+
+    return <aside className={cx('aside', { open: isOpen })} />;
+  }
+}
