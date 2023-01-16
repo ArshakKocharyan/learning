c31c1e241dc74efe03642e28462857471bb2af74
gosh888 <goshhayrapetyan@gmail.com>
2023-01-16 21:03:32 +0400

    test

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,28 @@ import React, { Component } from 'react';
 import { Aside } from './components/Aside/Aside';
 
 export class App extends Component {
+  state = {
+    isOpen: false,
+  };
+
+  handleToggleAside = () => {
+    const { isOpen } = this.state;
+    this.setState({ isOpen: !isOpen });
+  };
+
   render() {
+    const { isOpen } = this.state;
+
     return (
       <div>
-        <Aside isOpen />
+        <Aside isOpen={isOpen} />
+        <button
+          onClick={this.handleToggleAside}
+          className="btn"
+          type="button"
+        >
+          {isOpen ? 'Close Aside' : 'Open Aside'}
+        </button>
       </div>
     );
   }
